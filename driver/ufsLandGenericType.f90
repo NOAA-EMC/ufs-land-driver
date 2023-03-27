244ad1e2aee9a5ce0a3005e4ce23b2dc7280fadc
Michael Barlage <michael.barlage@noaa.gov>
2023-03-27 15:28:44 +0000

    add new output options to generic type structure

--- driver/ufsLandGenericType.f90
+++ driver/ufsLandGenericType.f90
@@ -6,45 +6,69 @@ implicit none
 
 type :: int1d
 
-  integer             , allocatable :: data (:)               ! 
-  logical                           :: output_flag  = .false. ! 
-  logical                           :: restart_flag = .false. ! 
-  character*128                     :: name                   ! 
-  character*128                     :: long_name              ! 
-  character*128                     :: units                  ! 
+  integer             , allocatable :: data (:)                    ! 
+  integer             , allocatable :: daily_mean (:)              ! 
+  integer             , allocatable :: monthly_mean (:)            ! 
+  integer             , allocatable :: solar_noon (:)              ! 
+  logical                           :: output_flag       = .false. ! 
+  logical                           :: restart_flag      = .false. ! 
+  logical                           :: daily_mean_flag   = .false. ! 
+  logical                           :: monthly_mean_flag = .false. ! 
+  logical                           :: solar_noon_flag   = .false. ! 
+  character*128                     :: name                        ! 
+  character*128                     :: long_name                   ! 
+  character*128                     :: units                       ! 
   
 end type int1d
 
 type :: real1d
 
-  real(kind=kind_phys), allocatable :: data (:)               ! 
-  logical                           :: output_flag  = .false. ! 
-  logical                           :: restart_flag = .false. ! 
-  character*128                     :: name                   ! 
-  character*128                     :: long_name              ! 
-  character*128                     :: units                  ! 
+  real(kind=kind_phys), allocatable :: data (:)                    ! 
+  real(kind=kind_phys), allocatable :: daily_mean (:)              ! 
+  real(kind=kind_phys), allocatable :: monthly_mean (:)            ! 
+  real(kind=kind_phys), allocatable :: solar_noon (:)              ! 
+  logical                           :: output_flag       = .false. ! 
+  logical                           :: restart_flag      = .false. ! 
+  logical                           :: daily_mean_flag   = .false. ! 
+  logical                           :: monthly_mean_flag = .false. ! 
+  logical                           :: solar_noon_flag   = .false. ! 
+  character*128                     :: name                        ! 
+  character*128                     :: long_name                   ! 
+  character*128                     :: units                       ! 
   
 end type real1d
 
 type :: int2d
 
-  integer             , allocatable :: data (:,:)             ! 
-  logical                           :: output_flag  = .false. ! 
-  logical                           :: restart_flag = .false. ! 
-  character*128                     :: name                   ! 
-  character*128                     :: long_name              ! 
-  character*128                     :: units                  ! 
+  integer             , allocatable :: data (:,:)                  ! 
+  integer             , allocatable :: daily_mean (:,:)            ! 
+  integer             , allocatable :: monthly_mean (:,:)          ! 
+  integer             , allocatable :: solar_noon (:,:)            ! 
+  logical                           :: output_flag       = .false. ! 
+  logical                           :: restart_flag      = .false. ! 
+  logical                           :: daily_mean_flag   = .false. ! 
+  logical                           :: monthly_mean_flag = .false. ! 
+  logical                           :: solar_noon_flag   = .false. ! 
+  character*128                     :: name                        ! 
+  character*128                     :: long_name                   ! 
+  character*128                     :: units                       ! 
   
 end type int2d
 
 type :: real2d
 
-  real(kind=kind_phys), allocatable :: data (:,:)             ! 
-  logical                           :: output_flag  = .false. ! 
-  logical                           :: restart_flag = .false. ! 
-  character*128                     :: name                   ! 
-  character*128                     :: long_name              ! 
-  character*128                     :: units                  ! 
+  real(kind=kind_phys), allocatable :: data (:,:)                  ! 
+  real(kind=kind_phys), allocatable :: daily_mean (:,:)            ! 
+  real(kind=kind_phys), allocatable :: monthly_mean (:,:)          ! 
+  real(kind=kind_phys), allocatable :: solar_noon (:,:)            ! 
+  logical                           :: output_flag       = .false. ! 
+  logical                           :: restart_flag      = .false. ! 
+  logical                           :: daily_mean_flag   = .false. ! 
+  logical                           :: monthly_mean_flag = .false. ! 
+  logical                           :: solar_noon_flag   = .false. ! 
+  character*128                     :: name                        ! 
+  character*128                     :: long_name                   ! 
+  character*128                     :: units                       ! 
   
 end type real2d
 
